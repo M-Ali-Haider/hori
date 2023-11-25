a516bba2a8fd2fee67c44344ccac6f23a908ee51
M-Ali-Haider <mah01134212100@gmail.com>
2023-11-25 22:52:53 +0500

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import HomePage from "./components/HomePage";
-import HowCanWe from "./components/HowCanWe";
 import TeamSlider from "./components/TeamSlider";
 import Footer from "./components/Footer";
 import ProjectSection from "./components/ProjectSection";
@@ -7,8 +5,6 @@ import './App.css';
 function App(){
   return(
     <>
-      <HomePage />
-      <HowCanWe />
       <TeamSlider/>
       <ProjectSection />
       <Footer />
